a097d1dbfa957b8747caea3b8ade05ab8f4a2b2d
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-28 20:46:28 +0700

    init url and setup url route

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/muhammadzhuhry/singkating-api-services/config"
+	"github.com/muhammadzhuhry/singkating-api-services/handlers"
 	"github.com/muhammadzhuhry/singkating-api-services/helper"
+	"github.com/muhammadzhuhry/singkating-api-services/routes"
 )
 
 func main() {
 	app := fiber.New()
+
+	// initialize handlers for url
+	urlHandler := handlers.NewUrlHandler()
+
+	// setup routes
+	routes.SetupURLRoute(app, urlHandler)
 
 	helper.Log.Info("Server started on port: " + config.Port)
 	app.Listen(":" + config.Port)
