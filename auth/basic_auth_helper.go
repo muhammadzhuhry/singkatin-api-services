82977bcef1c938aaef4dfe142806c335523a9a26
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-28 20:45:26 +0700

    create basic auth

---
+++ auth/basic_auth_helper.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"encoding/base64"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/muhammadzhuhry/singkating-api-services/config"
+	"github.com/muhammadzhuhry/singkating-api-services/helper"
+)
+
+func BasicAuthMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		username := config.BasicAuthUsername
+		password := config.BasicAuthPassword
+
+		authString := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+		authHeader := "Basic " + authString
+
+		if c.Get("Authorization") != authHeader {
+			res := helper.ErrorResponse(fiber.StatusUnauthorized, nil, "Unauthorized")
+			return helper.SendResponse(c, res)
+		}
+
+		return c.Next()
+	}
+}
