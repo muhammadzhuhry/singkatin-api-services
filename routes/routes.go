7b86e8e5f1f6b97cf7b28fd87ba7323da99185a9
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-30 12:40:39 +0700

    add api post url shorten

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 )
 
 func SetupURLRoute(app *fiber.App, urlHandler *handlers.UrlHandler) {
-	v1 := app.Group("/api/v1")
+	v1 := app.Group("/api/v1/url")
 	v1.Get("/", auth.BasicAuthMiddleware(), urlHandler.Root)
+	v1.Post("/shorten", auth.BasicAuthMiddleware(), urlHandler.UrlShorten)
 }
