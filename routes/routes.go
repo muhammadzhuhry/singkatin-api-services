93a1eae67950c04ca3461f407e8a86e57b870e4e
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-30 22:43:47 +0700

    create endpoint redirect url

--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,5 @@ func SetupURLRoute(app *fiber.App, urlHandler *handlers.UrlHandler) {
 	v1 := app.Group("/api/v1/url")
 	v1.Get("/", auth.BasicAuthMiddleware(), urlHandler.Root)
 	v1.Post("/shorten", auth.BasicAuthMiddleware(), urlHandler.UrlShorten)
+	v1.Get("/redirect", auth.BasicAuthMiddleware(), urlHandler.RedirectUrl)
 }
