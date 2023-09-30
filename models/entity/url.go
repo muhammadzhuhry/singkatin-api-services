8ae769204cdb15cb5c77f390d71fd367ae0de7f8
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-30 15:41:16 +0700

    change type of created at and expired at

--- models/entity/url.go
+++ models/entity/url.go
@@ -1,11 +1,9 @@
 package entity
 
-import "time"
-
 type Url struct {
-	ID        int       `json:"id"`
-	LongUrl   string    `json:"longUrl"`
-	ShortUrl  string    `json:"shortUrl"`
-	CreatedAt time.Time `json:"createdAt"`
-	ExpiredAt time.Time `json:"expiredAt"`
+	ID        int    `json:"id"`
+	LongUrl   string `json:"longUrl"`
+	ShortUrl  string `json:"shortUrl"`
+	CreatedAt string `json:"createdAt"`
+	ExpiredAt string `json:"expiredAt"`
 }
