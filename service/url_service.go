521566c6f0b2de23f6fd779a4184e04b9af1843d
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-30 15:53:03 +0700

    add timezone & logic check duplicate url

--- service/url_service.go
+++ service/url_service.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/muhammadzhuhry/singkating-api-services/config"
 	"github.com/muhammadzhuhry/singkating-api-services/helper"
 	"github.com/muhammadzhuhry/singkating-api-services/models"
 	"github.com/muhammadzhuhry/singkating-api-services/models/entity"
@@ -22,14 +23,21 @@ func NewUrlService(urlRepository *repository.UrlRepository) *UrlService {
 }
 
 func (s *UrlService) UrlShorten(c *fiber.Ctx, request request.Url) models.Response {
+	location := time.FixedZone(config.Timezone, 7*60*60)
+
 	payload := entity.Url{
 		LongUrl:   request.LongUrl,
 		ShortUrl:  helper.GenerateShortUrl(),
-		CreatedAt: time.Now(),
-		ExpiredAt: time.Now().Add(time.Hour * 24),
+		CreatedAt: time.Now().In(location).Format("2006-01-02 15:04:05"),
+		ExpiredAt: time.Now().In(location).Add(time.Hour * 24).Format("2006-01-02 15:04:05"),
 	}
 
-	// logic check duplicate short url and long url if exist return it
+	// logic check duplicate url
+	data, _ := s.UrlRepository.FindExistedUrl(payload.LongUrl)
+	if data != nil {
+		response := helper.ErrorResponse(fiber.StatusConflict, nil, "URL :"+payload.LongUrl+" has been shortened before")
+		return response
+	}
 
 	url, err := s.UrlRepository.InsertUrl(&payload)
 	if err != nil {
